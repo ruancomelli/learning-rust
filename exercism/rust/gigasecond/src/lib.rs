5e989e6ff9b0ef776e623602e9c4b182e2a81222
Ruan Comelli <ruancomelli@gmail.com>
2023-08-18 22:41:38 -0300

    refactor(exercise/gigasecond): use regular addition

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -3,5 +3,5 @@ use time::ext::NumericalDuration;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start.saturating_add(1_000_000_000_i64.seconds())
+    start + 1_000_000_000_i64.seconds()
 }
