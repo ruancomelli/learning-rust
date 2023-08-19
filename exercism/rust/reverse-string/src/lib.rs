cac623d94e5e39fff280d83329cf3bc0bf54be03
Ruan Comelli <ruancomelli@gmail.com>
2023-08-18 21:35:02 -0300

    refactor(exercise/reverse-string): use `Iterator::collect` instead of pushing to a string

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,12 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let graphemes = input.graphemes(true);
-    let mut output = String::new();
-
-    for c in graphemes.rev() {
-        output.push_str(c);
-    }
-
-    return output;
+    input.graphemes(true).rev().collect()
 }
