87ced5a6864a8f3159dbf4a5ec8c804d768a8799
Ruan Comelli <ruancomelli@gmail.com>
2023-08-18 22:59:08 -0300

    feat(clock): add initial basic interface to `Clock` and pass first test

--- exercism/rust/clock/src/lib.rs
+++ exercism/rust/clock/src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+
+#[derive(fmt::Debug, PartialEq, Eq)]
 pub struct Clock {
     hours: i32,
     minutes: i32,
@@ -5,10 +8,16 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock { hours: hours, minutes: minutes }
+        Clock { hours, minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         unimplemented!("Add {minutes} minutes to existing Clock time");
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
