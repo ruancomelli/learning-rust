4ccb9029c74d0705683d3bccc1b87e727264983a
Ruan Comelli <ruancomelli@gmail.com>
2023-08-18 21:29:42 -0300

    refactor(exercise/reverse-string): use the `graphemes` method from `str`

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,7 +1,7 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let graphemes = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
+    let graphemes = input.graphemes(true).collect::<Vec<&str>>();
     let mut output = String::new();
 
     for c in graphemes.iter().rev() {
