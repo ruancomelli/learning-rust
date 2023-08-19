63d4a3921c70cb149b795e64c8555eea0e47f3ed
Ruan Comelli <ruancomelli@gmail.com>
2023-08-18 23:28:10 -0300

    refactor(clock): simplify `Clock` structure to record only minutes

--- exercism/rust/clock/src/lib.rs
+++ exercism/rust/clock/src/lib.rs
@@ -1,30 +1,24 @@
 use std::fmt;
 
 const MINUTES_PER_HOUR: i32 = 60;
-const HOURS_PER_DAY: i32 = 24;
+const MINUTES_PER_DAY: i32 = 24 * MINUTES_PER_HOUR;
 
 
 #[derive(fmt::Debug, PartialEq)]
-pub struct Clock {
-    hours: i32,
-    minutes: i32,
-}
+pub struct Clock(i32);
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock {
-            hours: (hours + minutes.div_euclid(MINUTES_PER_HOUR)).rem_euclid(HOURS_PER_DAY),
-            minutes: minutes.rem_euclid(MINUTES_PER_HOUR),
-        }
+        Clock((hours * MINUTES_PER_HOUR + minutes).rem_euclid(MINUTES_PER_DAY))
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(self.hours, self.minutes + minutes)
+        Clock::new(0, self.0 + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.0 / MINUTES_PER_HOUR, self.0 % MINUTES_PER_HOUR)
     }
 }
