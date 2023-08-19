4ac40cf7be3a74090a5eaaed370cf6618642c793
Ruan Comelli <ruancomelli@gmail.com>
2023-08-18 21:21:28 -0300

    exercise(reverse-string): solve bonus exercise

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,9 +1,14 @@
+extern crate unicode_segmentation;
+
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
+    let graphemes = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
     let mut output = String::new();
 
-    for c in input.chars().rev() {
-        output.push(c);
+    for c in graphemes.iter().rev() {
+        output.push_str(c);
     }
-    
-    return output
+
+    return output;
 }
