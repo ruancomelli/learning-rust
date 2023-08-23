57f9c10c3f204e6f73621769bc184e43a7f6ef66
Ruan Comelli <ruancomelli@gmail.com>
2023-08-23 16:41:21 -0300

    fix(exercise/anagram): add case-insensitive, counter-based implementation

--- exercism/rust/anagram/src/lib.rs
+++ exercism/rust/anagram/src/lib.rs
@@ -1,17 +1,23 @@
 use std::collections::HashSet;
+use counter::Counter;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word_lower = word.to_lowercase();
+    let word_letter_counts = letter_counts(&word_lower);
+
     possible_anagrams
         .iter()
-        .filter(|&possible_anagram| is_anagram(word, possible_anagram))
+        .filter(|&possible_anagram| {
+            let possible_anagram_lower = possible_anagram.to_lowercase();
+
+            word_lower != possible_anagram_lower
+                && word_letter_counts == letter_counts(&possible_anagram_lower)
+        })
         .cloned()
         .collect()
 }
 
-fn is_anagram(word: &str, possible_anagram: &str) -> bool {
-    unimplemented!(
-        "Determine if '{possible_anagram}' is an anagram of '{word}'",
-        word = word,
-        possible_anagram = possible_anagram
-    );
+
+fn letter_counts(word: &str) -> Counter<char> {
+    word.chars().collect::<Counter<_>>()
 }
