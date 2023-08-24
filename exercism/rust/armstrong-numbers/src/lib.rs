6fb2fe01fea9ef3026bd7b397f10ef7c97927b20
Ruan Comelli <ruancomelli@gmail.com>
2023-08-23 21:50:40 -0300

    fix(exercise/armstrong-numbers): properly handle overflows

--- exercism/rust/armstrong-numbers/src/lib.rs
+++ exercism/rust/armstrong-numbers/src/lib.rs
@@ -1,11 +1,12 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    num == armstrong_sum(num)
-}
-
-fn armstrong_sum(num: u32) -> u32 {
     let digits = digits(num);
     let exponent = digits.len() as u32;
-    digits.iter().map(|digit| digit.pow(exponent)).sum()
+
+    if exponent >= 10 {
+        return false;
+    }
+
+    num == digits.iter().map(|digit| digit.pow(exponent)).sum()
 }
 
 fn digits(num: u32) -> Vec<u32> {
