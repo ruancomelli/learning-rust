bb6e2af3660c75ca1753a46bab79050740190ca4
Ruan Comelli <ruancomelli@gmail.com>
2023-08-23 21:53:00 -0300

    style(exercise/armstrong-numbers): improve formatting

--- exercism/rust/armstrong-numbers/src/lib.rs
+++ exercism/rust/armstrong-numbers/src/lib.rs
@@ -12,8 +12,7 @@ pub fn is_armstrong_number(num: u32) -> bool {
 fn digits(num: u32) -> Vec<u32> {
     if num < 10 {
         vec![num]
-    }
-    else {
+    } else {
         [vec![num % 10], digits(num / 10)].concat()
     }
 }
