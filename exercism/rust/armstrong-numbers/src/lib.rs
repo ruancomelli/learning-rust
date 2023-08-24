344358cf0f08a3073689e886e7fa6b85310801c1
Ruan Comelli <ruancomelli@gmail.com>
2023-08-23 21:45:20 -0300

    feat(exercise/armstrong-numbers): add first iteration

--- exercism/rust/armstrong-numbers/src/lib.rs
+++ exercism/rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {num} is an armstrong number")
+    num == armstrong_sum(num)
+}
+
+fn armstrong_sum(num: u32) -> u32 {
+    let digits = digits(num);
+    let exponent = digits.len() as u32;
+    digits.iter().map(|digit| digit.pow(exponent)).sum()
+}
+
+fn digits(num: u32) -> Vec<u32> {
+    if num < 10 {
+        vec![num]
+    }
+    else {
+        [vec![num % 10], digits(num / 10)].concat()
+    }
 }
