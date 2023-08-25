bde17109a80c563b329e7d89cc83285eca11a9f4
Ruan Comelli <ruancomelli@gmail.com>
2023-08-24 21:31:18 -0300

    refactor(exercise/prime-factors): iterate over possible candidates

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -1,17 +1,16 @@
 pub fn factors(n: u64) -> Vec<u64> {
     let mut factors = Vec::new();
     let mut n = n;
-    let mut candidate = 2;
-    let mut candidates = (3..).step_by(2);
+    let candidates = std::iter::once(2).chain((3..=n).step_by(2));
 
-    while n > 1 {
-        if n % candidate == 0 {
+    for candidate in candidates {
+        while n % candidate == 0 {
             n /= candidate;
             factors.push(candidate);
-        } else {
-            candidate = candidates.next().unwrap();
+        }
+        if n == 1 {
+            return factors;
         }
     }
-
-    factors
+    return factors;
 }
