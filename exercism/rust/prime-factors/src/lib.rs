25566ae68daac52c742930381cac1b8729333294
Ruan Comelli <ruancomelli@gmail.com>
2023-08-24 21:33:37 -0300

    refactor(exercise/prime-factors): use iterators for yielding candidates (#25)

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -1,16 +1,16 @@
 pub fn factors(n: u64) -> Vec<u64> {
     let mut factors = Vec::new();
     let mut n = n;
-    let mut i = 2;
+    let candidates = std::iter::once(2).chain((3..=n).step_by(2));
 
-    while i <= n {
-        if n % i == 0 {
-            n /= i;
-            factors.push(i);
-        } else {
-            i += 1;
+    for candidate in candidates {
+        while n % candidate == 0 {
+            n /= candidate;
+            factors.push(candidate);
+        }
+        if n == 1 {
+            return factors;
         }
     }
-
-    factors
+    return factors;
 }
