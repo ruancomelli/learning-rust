bcd8ffb5140020555bdca2ff0831fe8f753cd24d
Ruan Comelli <ruancomelli@gmail.com>
2023-10-07 15:19:57 -0300

    exercise: add `beer-song`

--- exercism/rust/proverb/src/lib.rs
+++ exercism/rust/proverb/src/lib.rs
@@ -1,14 +1,12 @@
 pub fn build_proverb(list: &[&str]) -> String {
     match list {
         [] => String::new(),
-        [first, rest @ ..] => {
-            list
-                .iter()
-                .zip(rest.iter())
-                .map(|(wanted_object, lost_object)| middle_sentence(wanted_object, lost_object))
-                .chain(std::iter::once(final_sentence(first)))
-                .collect()
-        }
+        [first, rest @ ..] => list
+            .iter()
+            .zip(rest.iter())
+            .map(|(wanted_object, lost_object)| middle_sentence(wanted_object, lost_object))
+            .chain(std::iter::once(final_sentence(first)))
+            .collect(),
     }
 }
 
