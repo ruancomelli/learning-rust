45aa99b93c452791fe5b9298fe8c12af70f1f2fe
Ruan Comelli <ruancomelli@gmail.com>
2024-03-26 14:48:12 -0300

    refactor(lib): improve chunk sizing and thread spawning

--- exercism/rust/parallel-letter-frequency/src/lib.rs
+++ exercism/rust/parallel-letter-frequency/src/lib.rs
@@ -3,36 +3,33 @@ use std::{collections::HashMap, ops::AddAssign, sync::mpsc, thread};
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let mut total_count = HashMap::<char, usize>::new();
 
+    if input.is_empty() {
+        return total_count;
+    }
+
     let receiver = {
         let (sender, receiver) = mpsc::channel();
 
-        let total_len = input.len();
-        let chunk_size =
-            total_len / worker_count + if total_len % worker_count == 0 { 0 } else { 1 };
+        thread::scope(|s| {
+            for chunk in input.chunks((input.len() - 1) / worker_count + 1) {
+                let thread_sender = sender.clone();
 
-        for index in 0..worker_count.min(total_len) {
-            let start = index * chunk_size;
-            let end = ((index + 1) * chunk_size).min(total_len);
-            let chunk = &input[start..end];
+                s.spawn(move || {
+                    let mut counter = HashMap::<char, usize>::new();
 
-            thread::scope(|s| {
-                for row in chunk {
-                    let thread_sender = sender.clone();
-
-                    s.spawn(move || {
-                        let mut counter = HashMap::<char, usize>::new();
-
+                    for row in chunk {
                         for c in row.chars().filter(|c| c.is_alphabetic()) {
                             if let Some(c) = c.to_lowercase().next() {
                                 counter.entry(c).or_insert(0).add_assign(1);
                             }
                         }
+                    }
 
-                        thread_sender.send(counter).unwrap();
-                    });
-                }
-            });
-        }
+                    thread_sender.send(counter).unwrap();
+                });
+            }
+        });
+
         receiver
     };
 
